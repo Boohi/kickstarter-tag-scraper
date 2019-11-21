c350db784714439a25f514d2ddc370e9744d2e82
Otto Mäenpää <otto.maenpaa@gmail.com>
2019-11-22 01:48:51 +0200

    Will make an JSON file out of all the data found. Done.

--- scraper.py
+++ scraper.py
@@ -1,40 +1,16 @@
-import urllib3
 import requests
-import pandas
-import csv
 import json
-from pandas.io.json import json_normalize
-""" import urllib """
+
 from bs4 import BeautifulSoup
 
 
-
-""" pandas.read_json(data).to_excel("test.xlsx") """
-
 data = []
 
-
-
-""" with open(data) as data_file:
-    test = json.load(data_file)
-
-df = json_normalize(test) """
-
-""" def flattenjson( b, delim ):
-    val = {}
-    for i in b.keys():
-        if isinstance( b[i], dict ):
-            get = flattenjson( b[i], delim )
-            for j in get.keys():
-                val[ i + delim + j ] = get[j]
-        else:
-            val[i] = b[i]
-
-    return val """
-page_query = 36
+page_query = 1
 amount = 1
 totalAmount = 0
 while (amount > 0):
+    print("Scraping page " + str(page_query))
     amount = 0
     quote_page = 'https://www.kickstarter.com/discover/advanced?woe_id=0&tag_id=121&sort=magic&ref=discovery_overlay&seed=2623960&page=' + str(page_query)
     page_query = page_query + 1
@@ -45,29 +21,8 @@ while (amount > 0):
         amount = amount + 1
         data.append(n['data-project'])
     totalAmount = totalAmount + amount
-
-with open(data) as fi:
-    data = json.load(fi)
-    df = pandas.DataFrame(data=data['rows'],columns=data['headings'])
-    df.to_csv('data_table.csv', index=False)
-
 print(totalAmount)
+with open('dataset.json', "w") as json_file:
+    json.dump(data, json_file)
 
-""" print(i) """
-""" pandas.read_json(data) """
-""" test = json.loads(data)
-f = csv.writer(open("test.csv", "wb+")) """
-"""     print(n['data-project'])
-    pandas.read_json(n['data-project']).to_excel("test.xlsx") """
-""" print(data) """
-""" with open("output.csv", "w", newline="") as f: """
-
-""" for i in data:
-    print(format(i))
-data = soup.div["data-project"] """
-
-""" print(data) """
-
-
-""" print(soup.prettify()) """
 
